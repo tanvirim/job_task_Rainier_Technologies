3a3a42ec37bc79b50191044c3cf67f33e4fa1536
Tanvir Imam Mitul <blackpanther141532@gmail.com>
2023-12-23 04:33:27 +0600

    theme correction

--- src/theme.js
+++ src/theme.js
@@ -8,30 +8,16 @@ const theme = extendTheme({
 
     useSystemColorMode: false,
   },
-  breakpoints: {
-    base: '375px',
-    md: '834px',
-    lg: '1440px',
-  },
-  sizes: {
-    container: {
-      base: '100%',
-      md: '80%',
-      lg: '70%',
-    },
-  },
+
   colors: {
-    // Define colors for the light mode
     light: {
       background: 'white',
       text: 'black',
-      // Define other colors as needed for the light mode
     },
-    // Define colors for the dark mode
+
     dark: {
       background: 'rgb(21,10,9)',
       text: 'white',
-      // Define other colors as needed for the dark mode
     },
   },
 });
